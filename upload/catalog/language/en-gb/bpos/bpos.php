8b6916ad9bea6425f6a0901f5457146f60cc5ec2
HP Web Design <51750567+hpwebdesign@users.noreply.github.com>
2025-09-12 21:54:57 +0700

    ...

--- upload/catalog/language/en-gb/bpos/bpos.php
+++ upload/catalog/language/en-gb/bpos/bpos.php
@@ -2,13 +2,13 @@
 
 $_['text_products']     = 'Products';
 $_['text_clear_all']    = 'Clear All';
-$_['text_print_bill']    = 'Print Bills';
+$_['text_checkout']     = 'Checkout';
 $_['text_view']         = 'View';
-$_['text_empty_cart']         = 'No items in cart';
-$_['text_confirm_logout']         = 'Confirm Logout';
-$_['text_confirm_to_logout']         = 'Are you sure you want to exit the POS System?';
-$_['text_cancel']         = 'Cancel';
-$_['text_logout']         = 'Logout';
+$_['text_empty_cart']   = 'No items in cart';
+$_['text_confirm_logout']     = 'Confirm Logout';
+$_['text_confirm_to_logout']  = 'Are you sure you want to exit the POS System?';
+$_['text_cancel']             = 'Cancel';
+$_['text_logout']             = 'Logout';
 $_['text_welcome']         = 'Welcome to BPOS!';
 $_['text_login_desc']         = 'Please sign in using your admin account to access the POS system.';
 $_['text_signin']         = 'Sign In';
